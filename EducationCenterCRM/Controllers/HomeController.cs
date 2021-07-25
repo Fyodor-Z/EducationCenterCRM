67c59913262f0bdfba1f536a95824d70a324da40
Fyodor-Z <fyodor.zakharov@gmail.com>
2021-07-26 01:22:49 +0300

    Add project files.

---
+++ EducationCenterCRM/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using EducationCenterCRM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
