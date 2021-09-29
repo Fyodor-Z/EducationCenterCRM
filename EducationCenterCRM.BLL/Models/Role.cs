1f7440215d689b5f5c609427f52e34774a89ed0b
Fyodor-Z <fyodor.zakharov@gmail.com>
2021-09-29 23:50:59 +0300

    Add Role Add User

---
+++ EducationCenterCRM.BLL/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationCenterCRM.BLL.Models
+{
+
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        public Role()
+        {
+            Users = new List<User>();
+        }
+    }
+}
---
+++ EducationCenterCRM.BLL/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationCenterCRM.BLL.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int? RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
