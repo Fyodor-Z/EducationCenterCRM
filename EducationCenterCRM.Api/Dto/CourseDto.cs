880d64b45b7dc8a7382597b0a6628019e7f9102d
Fyodor-Z <fyodor.zakharov@gmail.com>
2021-10-24 20:17:26 +0300

    add Controller add Swagger

---
+++ EducationCenterCRM.Api/Dto/CourseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.Api.Dto
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Program { get; set; }
+        public Guid TopicId { get; set; }
+        public string TopicName { get; set; }
+    }
+}
---
+++ EducationCenterCRM.Api/Mapper/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using EducationCenterCRM.Api.Dto;
+using EducationCenterCRM.BLL.Models;
+
+namespace EducationCenterCRM.Api.Mapper
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Course, CourseDto>()
+                .ForMember(model => model.TopicName, map => map.MapFrom(c => c.Topic.Title))
+                .ReverseMap();
+        }
+    }
+}
